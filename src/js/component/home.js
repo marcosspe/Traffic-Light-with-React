85d3e7b38d9728d8d136c1fac144109d5f0866a4
marcosspe <marcos.perez.garcia0@gmail.com>
2021-04-28 20:01:01 +0000

    new

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,18 +1,17 @@
 import React from "react";
-import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-    return (
-
-        <div>
-            <div id="barra"></div>
-            <div id="semaforo">
-            </div>
-        </div>);
-		
+	return (
+		<div>
+			<div>
+				<button className="btn bg-danger"></button>
+				<button className="btn bg-warning"></button>
+				<button className="btn bg-success"></button>
+			</div>
+		</div>
 	);
 }
