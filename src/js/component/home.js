86280e6eac28ac9e2536e53635e83a430ad8976e
marcosspe <marcos.perez.garcia0@gmail.com>
2021-04-28 18:36:26 +0000

    push2

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,21 +6,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	return (
-		
-			 <div>
-          <div id="barra"></div>
-          <div id="semaforo">
-            <div className= "luz roja" id= {this.state.estadoRoja} 
-            onClick={()=>this.setState({estadoRoja: "encendida", estadoAmarilla: "apagada", estadoVerde: "apagada"})}>
+    return (
+
+        <div>
+            <div id="barra"></div>
+            <div id="semaforo">
             </div>
-            <div className= "luz amarilla" id= {this.state.estadoAmarilla} 
-            onClick={()=>this.setState({estadoRoja: "apagada", estadoAmarilla: "encendida", estadoVerde: "apagada"})}>
-            </div>
-            <div className= "luz verde" id= {this.state.estadoVerde} 
-            onClick={()=>this.setState({estadoRoja: "apagada", estadoAmarilla: "apagada", estadoVerde: "encendida"})}>
-            </div>
-          </div>
         </div>);
 		
 	);
