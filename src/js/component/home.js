c9daf8983fd16a78428cc4fbed6e7872b4b849bd
marcosspe <marcos.perez.garcia0@gmail.com>
2021-04-28 18:28:17 +0000

    primer push

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +7,21 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		
+			 <div>
+          <div id="barra"></div>
+          <div id="semaforo">
+            <div className= "luz roja" id= {this.state.estadoRoja} 
+            onClick={()=>this.setState({estadoRoja: "encendida", estadoAmarilla: "apagada", estadoVerde: "apagada"})}>
+            </div>
+            <div className= "luz amarilla" id= {this.state.estadoAmarilla} 
+            onClick={()=>this.setState({estadoRoja: "apagada", estadoAmarilla: "encendida", estadoVerde: "apagada"})}>
+            </div>
+            <div className= "luz verde" id= {this.state.estadoVerde} 
+            onClick={()=>this.setState({estadoRoja: "apagada", estadoAmarilla: "apagada", estadoVerde: "encendida"})}>
+            </div>
+          </div>
+        </div>);
+		
 	);
 }
