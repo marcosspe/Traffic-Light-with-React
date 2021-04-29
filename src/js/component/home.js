e2ce9d2aa80b3bbf54e91bedb10267c3fe0961fd
marcosspe <marcos.perez.garcia0@gmail.com>
2021-04-29 16:07:26 +0000

    commit
    final

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./home.css";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -9,21 +8,24 @@ export function Home() {
 	const [color, setColor] = useState("");
 
 	return (
-		<div id="cuerpo" className={"container bg-dark pt-3 pb-3 "}>
-			<button
-				id="rojo"
-				className={color == "red2" ? color : "red"}
-				onClick={() => setColor("red2")}></button>
+		<div>
+			<div id="soporte" className={"container bg-dark pt-3 pb-3 "}></div>
+			<div id="cuerpo" className={"container bg-dark pt-3 pb-3 "}>
+				<button
+					id="rojo"
+					className={color == "red2" ? color : "red"}
+					onClick={() => setColor("red2")}></button>
 
-			<button
-				id="amarillo"
-				className={color == "yellow2" ? color : "yellow"}
-				onClick={() => setColor("yellow2")}></button>
+				<button
+					id="amarillo"
+					className={color == "yellow2" ? color : "yellow"}
+					onClick={() => setColor("yellow2")}></button>
 
-			<button
-				id="verde"
-				className={color == "green2" ? color : "green"}
-				onClick={() => setColor("green2")}></button>
+				<button
+					id="verde"
+					className={color == "green2" ? color : "green"}
+					onClick={() => setColor("green2")}></button>
+			</div>
 		</div>
 	);
 }
