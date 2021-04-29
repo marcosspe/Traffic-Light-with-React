2860417a289b0dc4b388e7de3c2280c8252e2ce3
marcosspe <marcos.perez.garcia0@gmail.com>
2021-04-29 01:50:01 +0000

    forma

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./home.css";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,11 +7,24 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div>
+		<div id="cuerpo" className="container bg-dark pt-3 pb-3">
 			<div>
-				<button className="btn bg-danger"></button>
-				<button className="btn bg-warning"></button>
-				<button className="btn bg-success"></button>
+				<button
+					id="rojo"
+					type="button"
+					className="btn btn-danger rounded-circle"></button>
+			</div>
+			<div>
+				<button
+					id="amarillo"
+					type="button"
+					className="btn btn-warning"></button>
+			</div>
+			<div>
+				<button
+					id="verde"
+					type="button"
+					className="btn btn-success"></button>
 			</div>
 		</div>
 	);
